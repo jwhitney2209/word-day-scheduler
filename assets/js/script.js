37df2524dfa3d3da2aef51d536e0086ae4dbb7a9
teachergg <jwhitney@bisd.us>
2022-03-10 01:20:13 -0600

    added HTML timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,20 @@
-var containerEl = document.querySelector(".container")
 
 var hourArr = ["9:00AM", 
-              "10:00AM", 
-              "11:00AM", 
-              "12:00PM", 
-              "1:00PM", 
-              "2:00PM", 
-              "3:00PM", 
-              "4:00PM", 
-              "5:00PM"];
-
-console.log(hourArr);
+"10:00AM", 
+"11:00AM", 
+"12:00PM", 
+"1:00PM", 
+"2:00PM", 
+"3:00PM", 
+"4:00PM", 
+"5:00PM"];
 
 // current date
 var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-console.log(currentDate);
 
-// 
+var createBlock = function () {
+
+}
+
+var textInput = $("<textarea>")
+  .addClass("col-12 col-md-9 description")
