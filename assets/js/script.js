b0cfa6d00f957da5620bfd52eae21da8f9b06c13
teachergg <jwhitney@bisd.us>
2022-03-12 13:26:09 -0600

    added loadTasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,35 @@
-
 var currentDate = moment().format('MMMM Do YYYY');
 $("#currentDay").html(currentDate);
 
-
-
 $('[data-hour-id').each(function(index) {
-  console.log(index + ": " + $(this).attr("id"));
+  // console.log(index + ": " + $(this).attr("id"));
   if (moment().hour() === parseInt($(this).attr("id"))) {
-    $(this).parent().addClass('present');
+    $(this).addClass('present');
   } else if (moment().hour() > parseInt($(this).attr("id"))) {
-    $(this).parent().addClass('past');
+    $(this).addClass('past');
   } else if (moment().hour() < parseInt($(this).attr("id"))) {
-    $(this).parent().addClass('future');
+    $(this).addClass('future');
   }
 })
 
+$('.saveBtn').click(function () {
+  // console.log($(this).prev().val());
+  var text = $(this).prev().val();
+  var time = $(this).prev().attr('id');
 
+  localStorage.setItem(time, text)
+})
+
+var loadTasks = function () {
+  $('#9').text(localStorage.getItem('9'));
+  $('#10').text(localStorage.getItem('10'));
+  $('#11').text(localStorage.getItem('11'));
+  $('#12').text(localStorage.getItem('12'));
+  $('#13').text(localStorage.getItem('13'));
+  $('#14').text(localStorage.getItem('14'));
+  $('#15').text(localStorage.getItem('15'));
+  $('#16').text(localStorage.getItem('16'));
+  $('#17').text(localStorage.getItem('17'));
+};
+
+loadTasks();
