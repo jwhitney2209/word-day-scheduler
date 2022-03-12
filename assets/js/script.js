742c75479ba49c5e034a2b98cdfc96269385f77c
teachergg <jwhitney@bisd.us>
2022-03-12 11:45:10 -0600

    added jQuery to change task color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,18 @@
 
-var hourEl = $('.hour');
-var containerEl = $('.container');
-var taskEl = $('#task');
-var saveBtnEl = $(".saveBtn");
-
 var currentDate = moment().format('MMMM Do YYYY');
 $("#currentDay").html(currentDate);
 
 
-var hourArr = [
-  {
-    dataId: 1,
-    hourId: 9,
-    hourText: "9:00AM"
-  },
-  {
-    dataId: 2,
-    hourId: 10,
-    hourText: "10:00AM"
-  },
-  {
-    dataId: 3,
-    hourId: 11,
-    hourText: "11:00AM"
-  },
-  {
-    dataId: 4,
-    hourId: 12,
-    hourText: "12:00PM"
-  },
-  {
-    dataId: 5,
-    hourId: 13,
-    hourText: "1:00PM"
-  },
-  {
-    dataId: 6,
-    hourId: 14,
-    hourText: "2:00PM"
-  },
-  {
-    dataId: 7,
-    hourId: 15,
-    hourText: "3:00PM"
-  },
-  {
-    dataId: 8,
-    hourId: 16,
-    hourText: "4:00PM"
-  },
-  {
-    dataId: 9,
-    hourId: 17,
-    hourText: "5:00PM"
+
+$('[data-hour-id').each(function(index) {
+  console.log(index + ": " + $(this).attr("id"));
+  if (moment().hour() === parseInt($(this).attr("id"))) {
+    $(this).parent().addClass('present');
+  } else if (moment().hour() > parseInt($(this).attr("id"))) {
+    $(this).parent().addClass('past');
+  } else if (moment().hour() < parseInt($(this).attr("id"))) {
+    $(this).parent().addClass('future');
   }
-];
+})
 
-var showSchedule = function () {
-  
-  // use for loop to append 9 timeblocks
-  for (var i = 0; i < 9; i++) {
-
-  }
-
-
-};
-
-var checkWhatHour = function () {
-  if (hourArr[i].hourId < currentHour) {
-    return "past col-12 col-md-9 description";
-  } else if (hourArr[i].hourId === currentHour) {
-    return "present col-12 col-md-9 description";
-  } else if (hourArr[i].hourId > currentHour) {
-    return "future col-12 col-md-9 description";
-  }
-};
-
-var currentHour = moment().hour();
-
-showSchedule();
 
